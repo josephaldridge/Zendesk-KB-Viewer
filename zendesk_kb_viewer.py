07c8c05b1eee46d7101ee5444df84e32268fabb3
Joey Aldridge <joeyaldridge@MacBookAir.lan>
2025-06-02 14:28:42 -0500

    Fix: Bind Flask to 0.0.0.0 and use PORT env for Render

--- zendesk_kb_viewer.py
+++ zendesk_kb_viewer.py
@@ -4,6 +4,7 @@ from requests.auth import HTTPBasicAuth
 from datetime import datetime, date
 import json
 import io
+import os
 
 app = Flask(__name__)
 
@@ -86,59 +87,5 @@ def export():
     return send_file(buffer, as_attachment=True, download_name='zendesk_articles.json', mimetype='application/json')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-from flask import Flask, render_template, request
-import requests
-from requests.auth import HTTPBasicAuth
-from datetime import datetime
-
-app = Flask(__name__)
-
-# Your Zendesk credentials
-ZENDESK_EMAIL = 'joseph.aldridge@libtax.com'
-ZENDESK_TOKEN = 'AedBgugdObrAKXm1iCjGIM8JlPvQFDCGGk9DZ4k3'
-ZENDESK_SUBDOMAIN = 'libertytax'
-
-ZENDESK_URL = f"https://{ZENDESK_SUBDOMAIN}.zendesk.com/api/v2/help_center/articles.json"
-
-def fetch_articles(from_date=None, to_date=None):
-    all_articles = []
-    url = ZENDESK_URL
-    auth = HTTPBasicAuth(f'{ZENDESK_EMAIL}/token', ZENDESK_TOKEN)
-
-    while url:
-        response = requests.get(url, auth=auth)
-        if response.status_code != 200:
-            break
-
-        data = response.json()
-        for article in data.get('articles', []):
-            created_at = datetime.fromisoformat(article['created_at'][:-1])
-
-            if from_date and created_at < from_date:
-                continue
-            if to_date and created_at > to_date:
-                continue
-
-            all_articles.append(article)
-
-        url = data.get('next_page')
-
-    return all_articles
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    articles = []
-    from_date_str = request.form.get('from_date')
-    to_date_str = request.form.get('to_date')
-
-    from_date = datetime.fromisoformat(from_date_str) if from_date_str else None
-    to_date = datetime.fromisoformat(to_date_str) if to_date_str else None
-
-    if request.method == 'POST':
-        articles = fetch_articles(from_date, to_date)
-
-    return render_template('index.html', articles=articles, from_date=from_date_str, to_date=to_date_str)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
